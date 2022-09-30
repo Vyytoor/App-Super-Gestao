82a9202392ba69340e3f82f7d599ada54263d184
Vyytoor <givanildovitor104@gmail.com>
2022-09-30 13:25:15 -0300

    Subindo projeto

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,18 +1,35 @@
 <h3> Fornecedores </h3>
 
-
-Fornecedor: {{ $fornecedores[0]['nome'] }}
-</br>
-Status: {{ $fornecedores[0]['status'] }}
-</br>
-
-@if (!($fornecedores[0]['status'] == 'S'))
-    Fornecedor Inativo
-@endif
+@php
 
 
-<br>
+@endphp
 
-@unless($fornecedores[0]['status'] == 'S')
-    Fornecedor Inativo
-@endunless
+
+@isset($fornecedores)
+    @forelse($fornecedores as $indice => $fornecedor )
+      Interação Atual: {{$loop -> iteration}}
+       <br>
+        Fornecedor: {{ $fornecedor['nome'] }}
+            </br>
+            Status: {{ $fornecedor['status'] }}
+            </br>
+            CNPJ: {{$fornecedor['cnpj'] ?? 'Dado não foi preenchido'}}
+            </br>
+            TELEFONE: ({{$fornecedor['ddd'] ?? ''}}) {{$fornecedor['ddd'] ?? ''}}
+            <br>
+            @if ($loop ->first)
+                Primeira interação com o Loop
+            @endif
+            @if ($loop ->last)
+                ultima interação com o Loop
+                    <br>
+                Total de Registro: {{$loop ->count}}
+            @endif
+
+        <hr>
+    @empty
+        Não existem fornecedores cadastrados
+    @endforelse
+
+@endisset
