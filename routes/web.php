43063589e24cfd70bc5e6ab2da4d59faf5ff0bde
Vyytoor <givanildovitor104@gmail.com>
2022-10-07 12:34:53 -0300

    2º Etapa, Colocando Extends, Includ, Section

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PrincipalController::class,'principal'])->name('site.index');
 Route::get('/contato', [ContatoController::class,'contato'])->name('site.contato');
+Route::post('/contato', [ContatoController::class,'contato'])->name('site.contato');
 Route::get('/sobrenos', [SobreNosController::class, 'sobrenos'])->name('site.sobrenos');
 Route::get('/login', function(){ return 'Login';})->name('site.login');
 
